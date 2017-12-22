dd4efd9add05a39cef2e2a670c3755cb3fde0a31
Loc Truong <loc@kiuglobal.com>
2017-12-23 00:56:44 +0700

    result

--- src/components/Results.js
+++ src/components/Results.js
@@ -3,6 +3,47 @@ import api from '../utils/api'
 import queryString from 'query-string'
 import Loading from './Loading'
 import {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
+import PlayerPreview from './PlayerPreview'
+
+
+function Profile (props) {
+  var info = props.info
+
+  return (
+    <PlayerPreview username={info.login} image={info.avatar_url}>
+      <ul className='space-list-items'>
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}>{info.blog}</a></li>}
+      </ul>
+    </PlayerPreview>
+  )
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired,
+}
+
+function Player (props) {
+  return (
+    <div>
+      <h1 className='header'>{props.label}</h1>
+      <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <Profile info={props.profile} />
+    </div>
+  )
+}
+
+Player.propTypes = {
+  label: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  profile: PropTypes.object.isRequired,
+}
 
 
 export default class Results extends React.Component {
@@ -72,7 +113,18 @@ export default class Results extends React.Component {
 
     return (
       <div>
-        Results
+        <div className='row'>
+          <Player
+            label='Winner'
+            score={winner.score}
+            profile={winner.profile}
+          />
+          <Player
+            label='Loser'
+            score={loser.score}
+            profile={loser.profile}
+          />
+        </div>
       </div>
     )
     
